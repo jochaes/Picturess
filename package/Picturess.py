2300060db15955bde011f522acde73e449860785
Josué Chaves Araya <d4n11083@estudiantec.cr>
2022-06-03 21:50:51 -0600

    Did not work

--- package/Picturess.py
+++ package/Picturess.py
@@ -158,16 +158,18 @@ class MyCompressor:
 
 #https://stackoverflow.com/questions/31836104/pyinstaller-and-onefile-how-to-include-an-image-in-the-exe-file
 
-def resource_path(relative_path):
-    try:
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
-    return os.path.join(base_path, relative_path)
+# def resource_path(relative_path):
+#     try:
+#         base_path = sys._MEIPASS
+#     except Exception:
+#         base_path = os.path.abspath(".")
+#     return os.path.join(base_path, relative_path)
 
 
-wtmarkPath = resource_path("logo_w_letters.png")
-#wtmarkPath = "../watermark/logo_w_letters.png"
+# wtmarkPath = resource_path("logo_w_letters.png")
+# wtmarkPath = "../watermark/logo_w_letters.png"
+
+wtmarkPath = os.path.join(os.path.dirname(__file__), "images/logo_with_letters.png")
 
 #messagebox.showerror('Python Error', wtmarkPath)
 wtmrk = MyWatermark("","", wtmarkPath)
